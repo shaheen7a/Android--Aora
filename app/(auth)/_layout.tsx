1d5d5bae22e7cfebcdcd8b7f7a7bbbe9595bc73f
Shaheen7a <aousshaheen7@gmail.com>
2024-05-30 23:31:15 +0300

    file creating

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,14 +1,12 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 
-const profile = () => {
+const AuthLayout = () => {
   return (
     <View>
-      <Text>profile</Text>
+      <Text>AuthLayout</Text>
     </View>
   );
 };
 
-export default profile;
-
-const styles = StyleSheet.create({});
+export default AuthLayout;
