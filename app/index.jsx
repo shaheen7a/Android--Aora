a820beeee72b912c89d68593bed1d8dd07303787
Shaheen7a <aousshaheen7@gmail.com>
2024-06-03 22:14:11 +0300

    nativewind

---
+++ app/index.jsx
@@ -0,0 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-3xl">This is the Entry Point of our app</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go To Profile
+      </Link>
+    </View>
+  );
+}
